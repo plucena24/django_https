aa8fbb438002d5bb847706a35df971ca693869cf
NAThompson <nickadamthompson@gmail.com>
2015-07-26 17:41:46 +0000

    Update the nginx.conf for higher grade on SSLLabs

--- src/gunicorn_config.py
+++ src/gunicorn_config.py
@@ -1,6 +1,13 @@
 import multiprocessing
 
 bind = "127.0.0.1:8000"
+
 workers = multiprocessing.cpu_count()*2 + 1
-certfile = '../lib/python3.4/site-packages/sslserver/certs/development.crt'
-keyfile =  '../lib/python3.4/site-packages/sslserver/certs/development.key'
+
+#The .crt and .key here must be exactly the same as in the nginx.conf or else 501
+certfile = '/home/NAThompson/jeremiahcornsticks.com.ssl/jeremiahcornsticks_com.crt'
+keyfile  = '/home/NAThompson/jeremiahcornsticks.com.ssl/jeremiahcornsticks.com.key'
+
+# sslserver generates self-signed keys for testing:
+#certfile = '../lib/python3.4/site-packages/sslserver/certs/development.crt'
+#keyfile =  '../lib/python3.4/site-packages/sslserver/certs/development.key'
